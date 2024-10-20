4277c5dc22df6855b50ffed8b81c5bcea8eefd3b
Radih P Paraguas Jr <radihjrprgs@gmail.com>
2024-10-20 17:21:30 +0800

    configure mypy, ruff, and isort

--- bayc_nft_transfer_monitor/wsgi.py
+++ bayc_nft_transfer_monitor/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bayc_nft_transfer_monitor.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bayc_nft_transfer_monitor.settings")
 
 application = get_wsgi_application()
